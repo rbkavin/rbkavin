7c553af008bcb6ab082ec81f07f221fa623b975f
kavin kumar <bkavin001@gmail.com>
2021-07-07 15:58:34 +0530

    options

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,3 +1,44 @@
+const myColorPallet = {
+    red: "#e8505b",
+    blue: "#467fe7",
+    white: "#ffffff",
+    darkRed: "#a0373f"
+}
+
+const projectLinks = [{
+        type: "",
+        platform: "Web AR",
+        link: "",
+        videoLink: "",
+    },
+    {
+        type: "",
+        platform: "Facebook AR",
+        link: "",
+        videoLink: "",
+    },
+    {
+        type: "",
+        platform: "Instagram AR",
+        link: "",
+        videoLink: "",
+    },
+    {
+        type: "",
+        platform: "Snapchat AR",
+        link: "",
+        videoLink: "",
+    },
+
+    // {
+    //     type: "",
+    //     platform: "",
+    //     link: "",
+    //     videoLink: "",
+    // }
+]
+
+
 function setAppleFavicons() {
     let headTitle = document.querySelector('head');
 
@@ -41,7 +82,6 @@ function setAppleFavicons() {
         headTitle.appendChild(setAFavicon);
     });
 }
-setAppleFavicons();
 const links = [{
         title: "Instagram portfolio",
         link: "https://www.facebook.com/sparkarhub/portfolios/ig/rbkavin/"
@@ -51,57 +91,78 @@ const links = [{
         link: "https://lensstudio.snapchat.com/creator/3dRaqaIS7GMYJrLO4omM5w"
     },
 
-    //         {
-    // title:"",
-    // link:""
-    //         },
+
 ];
-// links.forEach((i) => {
-//     createButton(document.getElementById("linkDiv"), i.link, i.title)
 
-// })
+function init() {
+    setAppleFavicons();
+    createPortfolio("All");
+    menuOption();
+}
+init();
 
-function createButton(main, link_, title_) {
+//create menu options
+function menuOption() {
+    const options = [...new Set(projectLinks.map(a => a.platform))]
+    options.push("All");
+    options.sort();
+    // console.log(options)
+    options.forEach((value) => {
+        const dropDown = document.getElementsByClassName("dropdown-content")[0]
+        let div = document.createElement("div");
+        div.id = value;
+        div.onclick = function () {
+            createPortfolio(this.id)
+            // console.log(this);
+        };
+        // window.addEventListener("click",)
+        div.appendChild(document.createTextNode(value))
+        dropDown.appendChild(div);
 
-    var wraper = document.createElement('div');
-    wraper.className = "wrapers";
-    var a = document.createElement('a');
-    a.href = link_;
-    var button = document.createElement('button');
-    button.className = "pushable"
-    var span1 = document.createElement('span');
-    span1.className = 'shadow';
-    var span2 = document.createElement('span');
-    span2.className = 'edge';
+    })
 
-    var span3 = document.createElement('span');
-    span3.className = 'front';
 
-    var link = document.createTextNode(title_);
-    span3.appendChild(link)
-
-    button.appendChild(span1)
-    button.appendChild(span2)
-    button.appendChild(span3)
-    if (link_ == null) {
-
-        wraper.appendChild(button);
-
-    } else {
-        a.appendChild(button)
-        wraper.appendChild(a);
-    }
-
-    main.appendChild(wraper);
 }
 
-const projectLinks = [{
-    type: "",
-    platform: "",
-    link: "",
-    videoLink: "",
-}]
-// const wraper = document.getElementById("portfolioWraper");
-// for (let i = 0; i <= 10; i++) {
 
-// }
+//to create portfolio
+function createPortfolio(catagory) {
+
+
+    const wraper = document.getElementsByClassName("flex-container")[0];
+    wraper.innerHTML = "";
+
+    menuSelected(catagory);
+
+    projectLinks.forEach(a => {
+        let print = false;
+        if (a.platform === catagory) {
+            print = true;
+        } else if (catagory === "All") {
+
+            print = true;
+        } else {
+            print = false;
+        }
+        if (print) {
+            createCards(wraper, a.platform);
+        }
+    })
+
+}
+
+function createCards(wraper, i) {
+    let div = document.createElement("div");
+    let text = document.createTextNode(i + "");
+
+    div.appendChild(text);
+    wraper.appendChild(div);
+}
+
+function menuSelected(name) {
+    const button = document.getElementById("buttonName");
+    button.innerHTML = "";
+    let text = document.createTextNode(name);
+    button.appendChild(text);
+
+}
